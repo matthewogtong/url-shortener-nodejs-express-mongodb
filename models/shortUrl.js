038d5f688a73c6a1f2ad9659c1b6161a1158a729
matthewogtong <ogtongm@gmail.com>
2021-08-12 23:45:47 -0400

    add full and short Url to schema

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose')
 
-const shortURLScheme = new mongoose.Schema
+const shortURLScheme = new mongoose.Schema({
+    full: {
+        type: String,
+        require: true
+    },
+    short: {
+        type: String,
+        required: true
+    }
+})
