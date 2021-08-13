26930ba3547a533821fb11ae1343dd05c8bc68bd
matthewogtong <ogtongm@gmail.com>
2021-08-12 23:40:45 -0400

    connect mongodb database and add useUnifiedtopology and useNewUrlParser to avoid deprecation warnings

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 
@@ -8,7 +13,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/shortUrls', (req, res) => {
-    
+
 })
 
 app.listen(process.env.PORT || 5000)
