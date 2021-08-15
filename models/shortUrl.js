b68b2cb46336ea23283b00b008c3f31462e3bb8f
matthewogtong <ogtongm@gmail.com>
2021-08-15 19:27:47 -0400

    finish url shortener with one error, create .gitignore file for node modules

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose')
 const shortId = require('shortid')
 
-const shortURLScheme = new mongoose.Schema({
-    full: {
-        type: String,
-        require: true
-    },
-    short: {
-        type: String,
-        required: true,
-        default: shortId.generate //automatically run generate function so no wrap needed
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0
-    }
-}) 
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+})
 
-module.exports = mongoose.model('ShortUrl, shortUrlSchema')
+module.exports = mongoose.model('ShortUrl', shortUrlSchema)
--- server.js
+++ server.js
@@ -4,18 +4,31 @@ const ShortUrl = require('./models/shortUrl')
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
-    useNewUrlParser: true, useUnifiedTopology: true
+  useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
 })
 
-app.post('/shortUrls', (req, res) => {
-    ShortUrl.create({ full: req.body.fullUrl })
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
 })
 
-app.listen(process.env.PORT || 5000)
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000);
